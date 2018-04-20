7db4ed85382d714f932dc2f25987eb6e3cb0c751
Douglas Alves <douglas.expalves@gmail.com>
2018-04-20 12:59:52 -0300

    add firebase config import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import 'vuetify/dist/vuetify.min.css';
-import '@/service/firebase';
+import firebase from '@/service/firebase';
 
 import Vue from 'vue';
 import Vuetify from 'vuetify';
@@ -11,15 +11,22 @@ import store from '@/store';
 import App from './App';
 import router from './router';
 
-
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  store,
-  render: h => h(App),
+const unsubscribe = firebase.auth()
+.onAuthStateChanged((user) => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    render: h => h(App),
+    created () {
+      if (user) {
+        store.dispatch('autoSignIn', { email: sessionStorage.getItem('email') });
+      };
+    },
+  });
+  unsubscribe();
 });
