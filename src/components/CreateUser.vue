48c7a1073aac80ca135d2a3fc53174a4da896f38
Douglas Alves <douglas.expalves@gmail.com>
2018-04-20 13:37:40 -0300

    update cancel button

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -44,7 +44,7 @@
             </v-flex>            
             <v-flex class="text-xs-center" mt-5>
               <v-btn color="primary" outline type="submit" :disabled="!formValid || loading">Create</v-btn>
-              <v-btn color="warning" :to="{ name: 'Index' }" outline :disabled="loading">Cancel</v-btn>
+              <v-btn color="error" :to="{ name: 'Index' }" outline :disabled="loading">Cancel</v-btn>
             </v-flex>
             <v-flex xs12 class="text-xs-center" v-if="loading" mt-5>
               <v-progress-circular indeterminate color="primary"></v-progress-circular>
